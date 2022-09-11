8fce62842e181acbaeeb6c04c06ce930a436bf44
Keith Tauscher <keta6457@colorado.edu>
2022-09-10 23:26:24 -0600

    Made package pip installable from source

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='cubetime',
+    version='0.1.0',    
+    description='A multi-segment terminal-based timer',
+    url='https://github.com/ktausch/cubetime',
+    author='Keith Tauscher',
+    author_email='Keith.Tauscher@gmail.com',
+    license='MIT',
+    packages=['cubetime'],
+    install_requires=[
+        "click",
+        "fastparquet",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "pyyaml"
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cubetime = cubetime.app:main"
+        ]
+    },
+)
